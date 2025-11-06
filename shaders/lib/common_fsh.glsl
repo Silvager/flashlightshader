1123f4f8ac1593c990a7d8a8e0dab7d323bfb0c7
Silvager <skylar.guillaume@gmail.com>
2025-11-05 19:25:22 -0500

    Fix bug with new dark thing

--- shaders/lib/common_fsh.glsl
+++ shaders/lib/common_fsh.glsl
@@ -6,19 +6,13 @@ void makeTexturesBw(inout vec4 newVertexColor, inout vec4 texColor, float percen
     newVertexColor = vec4(((newVertexColor.rgb * (1-percentDarkened))+blackWhiteVertexCol), newVertexColor.a);
     texColor = vec4(((texColor.rgb * (1-percentDarkened))+blackWhiteTexCol), texColor.a);
 }
-// void makeTexturesBw(inout vec4 newVertexColor, inout vec4 texColor) {
-//     float blackWhiteTexCol = (texColor.r+texColor.g+texColor.b)/3.0;
-//     float blackWhiteVertexCol = (newVertexColor.r+newVertexColor.g+newVertexColor.b)/3.0;
-//     newVertexColor = vec4(blackWhiteVertexCol, blackWhiteVertexCol, blackWhiteVertexCol, newVertexColor.a);
-//     texColor = vec4(blackWhiteTexCol, blackWhiteTexCol, blackWhiteTexCol, texColor.a);
-// }
 vec4 getDarkenedLightColor(vec4 lightColor, float vertexDistance, float moonLighting) {
     float lightBrightness = (lightColor.r+lightColor.g+lightColor.b)/3.0;
-    if (lightBrightness < 0.2) { //If in absolute darkness, see zilch
+    if (lightBrightness < 0.1) { //If in absolute darkness, see zilch
         lightBrightness = 0.0;
-    } else if (lightBrightness < 0.5) {
+    } else if (lightBrightness < 0.52) {
         float darkBrightnessMult = smoothstep(1.0, 0.0, (vertexDistance/(moonLighting*50)));
-        float fadeDarknessMult = smoothstep(0.38, 0.5, lightBrightness);
+        float fadeDarknessMult = smoothstep(0.38, 0.52, lightBrightness);
         if (darkBrightnessMult > fadeDarknessMult) {
             lightBrightness *= darkBrightnessMult;
         } else {
@@ -46,26 +40,18 @@ vec3 fogColor
     vec4 lightColor = texture(lightmap, lightCoord);
     
     vec4 newVertexColor = vertexColor;
-    // if (flashlightLightStrength == 0.0) {
-    //             makeTexturesBw(newVertexColor, texColor);
-    //         }
+
     if (MEGA_DARKNESS_ENABLED == 1) {
         lightColor = getDarkenedLightColor(lightColor, vertexDistance, moonLighting);
     }
     if (flashlightLightStrength > 0.0) {
-        vec4 modifiedFlashlightColor = vec4(flashlightColor.rgb*flashlightLightStrength, 0.01);
-        if ((lightColor.r + lightColor.g + lightColor.b)/3.0 < 0.4 && MEGA_DARKNESS_ENABLED == 1) {
-            modifiedFlashlightColor *= smoothstep(0.0, 1.0, (1.0-(vertexDistance/FLASHLIGHT_DISTANCE)));
-
-        }
-    lightColor += modifiedFlashlightColor;
+        vec4 modifiedFlashlightColor = vec4(flashlightColor.rgb*flashlightLightStrength, 0.1);
+        lightColor.rgb += modifiedFlashlightColor.rgb;
     }
+    //Make it black and white scaling from fully at 0.2 light and none at 0.4 light
     float lightBrightness = (lightColor.r + lightColor.g + lightColor.b)/3.0;
     if (lightBrightness < 0.4) {
-        //0.3 should be 0 0.4 is 1;
-        float bwAmount = (lightBrightness * 10)+0.3;
         makeTexturesBw(newVertexColor, texColor, smoothstep(0.4, 0.2, lightBrightness));
-        // makeTexturesBw(newVertexColor, texColor, 1.0);
     }
     vec4 unFogColor = texColor * newVertexColor * lightColor;
     if (vertexDistance > fogStart) {
